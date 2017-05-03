29d6e017c2100e48b2587434ca6fa1e9b9d80a20
Priyanka <priyanka_ekbote@mindtree.com>
2017-05-03 15:27:41 +0530

    Made all changes.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var app=express();
+
+var routes=require('./routes/route.js');
+
+app.set('view engine','jade');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',routes.home);
+app.get('/:city',routes.city);
+
+var port = process.env.PORT || 3002;
+
+var server=app.listen(port,function(req,res){
+    console.log("Catch the action at http://localhost:"+port);
+});
+
+module.exports = app;
